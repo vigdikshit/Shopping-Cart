2a528a6e3722e97fa6fa26f42f1f7cd08d79ec3e
vigdikshit <dikshit.vig@gmail.com>
2019-04-18 20:50:43 -0400

    Step 1: Shopping Cart(updated: input from user)

--- ShoppingCart/src/dikshit/shoppingCart/Checkout.java
+++ ShoppingCart/src/dikshit/shoppingCart/Checkout.java
@@ -1,7 +1,9 @@
 package dikshit.shoppingCart;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Checkout {
 	public static void main(String args[]) {
@@ -13,13 +15,20 @@ public class Checkout {
 		products.addItems("apple", 0.6);
 		products.addItems("orange", 0.25);
 
-		list.add("apple");
-		list.add("apple");
-		list.add("orange");
-		list.add("apple");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter item: ");
+		String s = scan.nextLine();
+		String arr[] = s.split(",");
+		
+		list = Arrays.asList(arr);
+//		list.add("apple");
+//		list.add("apple");
+//		list.add("orange");
+//		list.add("apple");
 		
 		double totalCost = calPrice.calculate(list, products.getItems());
 
 		System.out.println(totalCost + "£");
+		scan.close();
 	}
 }
